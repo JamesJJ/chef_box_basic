0a3b8ba5eaece0dcd02ef4f19b3a4ff0ea919901
JamesJJ <James_Hooper@htc.com>
2015-01-06 12:57:42 +0800

    add yum-utils to default package set

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 default["box_basic"]["set_shell_preferences"] = true
 default["box_basic"]["use_cloudinit_hostname"] = false
 
-default["box_basic"]["install_packages"] = [ "bind-utils","traceroute","telnet","screen","ruby","rubygems","git", "python-pip", "ntp" ]
+default["box_basic"]["install_packages"] = [ "yum-utils","bind-utils","traceroute","telnet","screen","ruby","rubygems","git", "python-pip", "ntp" ]
 
 # CENTOS6 default["box_basic"]["yum_groups_remove"] = [ "X Window System","Desktop Debugging and Performance Tools","MySQL Database client","NFS file server","Network Infrastructure Server","Office Suite and Productivity","Internet Browser","Dial-up Networking Support","Print Server","Printing client","Web Server","E-mail server","Electronic Lab","Fonts","General Purpose Desktop","Graphical Administration Tools","Internet Applications","Java Platform","Directory Client","Legacy X Window System compatibility" ]
 default["box_basic"]["yum_groups_remove"] = []
