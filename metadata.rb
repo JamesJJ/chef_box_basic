95517aa49de49f6635fae1b9b6a73e68b2f370b3
JamesJJ <James_Hooper@htc.com>
2015-02-04 13:43:44 +0800

    yum -y

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'github@jamesjj.net'
 license          'MIT'
 description      'Installs/Configures instances'
 long_description      'Installs/Configures instances'
-version          '0.2.3'
+version          '0.2.4'
 
 depends 'users'
 depends 'chef-solo-search'
